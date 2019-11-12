51235289ef99279f2d50823b9fe888b35a02bc79
Tony Alpha <anton.sherri@gmail.com>
2019-11-12 14:17:46 +0100

    classes & interfaces setup

---
+++ src/main/java/be/antonsherri/opdracht1/housekeeping/services/CleaningTool.java
@@ -0,0 +1,5 @@
+package be.antonsherri.opdracht1.housekeeping.services;
+
+public interface CleaningTool {
+    public void doCleanJob();
+}
---
+++ src/main/java/be/antonsherri/opdracht1/housekeeping/services/Sponge.java
@@ -0,0 +1,7 @@
+package be.antonsherri.opdracht1.housekeeping.services;
+
+public class Sponge implements CleaningTool {
+    public void doCleanJob() {
+        System.out.println("Spunge spunge");
+    }
+}
